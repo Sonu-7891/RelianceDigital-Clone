e4a734a9cc8b4371cacd945620d424438f1373db
Sonu-7891 <sonuverma789163@gmail.com>
2025-05-19 23:16:01 +0530

    code backend url update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api" || "https://reliancedigital-clone-backend.onrender.com/api",
+  baseURL: "https://reliancedigital-clone-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
